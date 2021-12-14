0aee8610de2837cc186980e3b0b95b52f16369d7
VictorMacabu <54696589+VictorMacabu@users.noreply.github.com>
2021-12-14 10:29:02 -0300

    iniciando projeto

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1> {{ titulo }} </h1>
+    <img src="" alt="">
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+
+  data () {
+    return{
+      titulo: 'meu Album',
+      foto: {
+        img: '',
+        descricao: ''
+      }
+    }
+}
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
